89a463dc3200c39ecc84e35ee85a4d226e5be841
Ricardo Signes <rjbs@cpan.org>
2009-11-29 10:14:01 -0500

    now we work... and perform serious abuses

--- lib/Dist/Zilla/Plugin/TaskWeaver.pm
+++ lib/Dist/Zilla/Plugin/TaskWeaver.pm
@@ -3,6 +3,7 @@ use Moose;
 extends 'Dist::Zilla::Plugin::PodWeaver';
 
 use Moose::Autobox;
+use Pod::Weaver::Plugin::TaskWeaver;
 
 # need an attr for "plugin name to put this after" -- ugh! -- or a coderef to
 # find the first plugin where the coderef tests true; useful for "generic and
@@ -16,11 +17,25 @@ around weaver => sub {
   my $plugin = Pod::Weaver::Plugin::TaskWeaver->new({
     plugin_name => 'TaskWeaver',
     weaver      => $weaver,
+    zillaplugin => $self,
   });
 
-  $weaver->plugins->push($plugin);
+  my ($i) =
+    grep { $weaver->plugins->[$_]->isa('Pod::Weaver::Section::Leftovers') }
+    ($weaver->plugins->keys->flatten);
+
+  splice @{ $weaver->plugins }, $i, 0, $plugin;
 
   return $weaver;
 };
+
+has prereq => (
+  is  => 'ro',
+  isa => 'HashRef',
+  init_arg => undef,
+  default  => sub { {} },
+);
+
+with 'Dist::Zilla::Role::FixedPrereqs';
 
 1;
--- lib/Pod/Weaver/Plugin/TaskWeaver.pm
+++ lib/Pod/Weaver/Plugin/TaskWeaver.pm
@@ -1,17 +1,79 @@
 package Pod::Weaver::Plugin::TaskWeaver;
 use Moose;
-with 'Pod::Weaver::Role::Transformer';
+with 'Pod::Weaver::Role::Dialect';
 with 'Pod::Weaver::Role::Section';
 
-# TRANSFORMER: find all =pkgroup and collect =pkg and flat under them
-#              convert =pkgroup to head1, =pkg to head2
+use Moose::Autobox;
+use Pod::Elemental::Selectors -all;
+use Pod::Elemental::Transformer::Nester;
+
+# DIALECT: find all =pkgroup and collect =pkg and flat under them
+#          convert =pkgroup to head1, =pkg to head2
 #         
-# SECTION:     register packages with prereq (no $zilla until now)
-#              include all prereqs
+# SECTION: register packages with prereq (no $zilla until now)
+#          include all prereqs
+
+has zillaplugin => (
+  is  => 'ro',
+  isa => 'Dist::Zilla::Plugin::TaskWeaver',
+  required => 1,
+);
+
+sub record_prereq {
+  my ($self, $pkg, $ver) = @_;
+  $self->zillaplugin->prereq->{$pkg} = $ver;
+}
+
+sub translate_dialect {
+  my ($self, $document) = @_;
+
+  my $nester = Pod::Elemental::Transformer::Nester->new({
+    top_selector => s_command([ qw(pkgroup) ]),
+    content_selectors => [
+      s_flat,
+      s_command( [ qw(pkg over item back) ]),
+    ],
+  });
+
+  $nester->transform_node($document);
+
+  return;
+}
 
 sub weave_section {
   my ($self, $document, $input) = @_;
 
+  my $input_pod = $input->{pod_document};
+
+  my @pkgroups;
+  for my $i (reverse $input_pod->children->keys->flatten) {
+    my $child = $input_pod->children->[ $i ];
+    unshift @pkgroups, splice(@{$input_pod->children}, $i, 1)
+      if  $child->does('Pod::Elemental::Command')
+      and $child->command eq 'pkgroup';
+  }
+
+  for my $pkgroup (@pkgroups) {
+    $pkgroup->command('head2');
+
+    for my $child (@{ $pkgroup->children }) {
+      next unless $child->does('Pod::Elemental::Command')
+           and    $child->command eq 'pkg';
+
+      $child->command('head3');
+      
+      my ($pkg, $ver) = split /\s+/, $child->content;
+      $self->record_prereq($pkg, $ver);
+    }
+  }
+
+  my $section = Pod::Elemental::Element::Nested->new({
+    command  => 'head1',
+    content  => 'TASK CONTENTS',
+    children => \@pkgroups,
+  });
+
+  $input_pod->children->unshift($section);
 
   return;
 }
